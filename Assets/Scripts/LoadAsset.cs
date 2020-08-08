4b844e2418e15274662bf6e060fc8b09a7582b88
Johlver Pardo <jjpardo2002@gmail.com>
2020-08-08 16:35:14 -0500

    Test

---
+++ Assets/Scripts/LoadAsset.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using System.IO;
+public class LoadAsset : MonoBehaviour {
+    UnityWebRequest www;
+    private void Start()
+    {
+  
+    }
+    /// <summary>
+    /// Function Instantiate Asset from AssetBundle
+    /// </summary>
+    /// <param name="objeto">Name Object to instantiate</param>
+    /// <param name="bundleName">Name Bundle</param>
+    public void LoadLocal(string objeto, string bundleName)
+    {
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(
+            Path.Combine(Application.streamingAssetsPath, "StandaloneWindows", bundleName));
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+
+        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>(objeto);
+        Instantiate(prefab);
+
+        myLoadedAssetBundle.Unload(false);
+    }
+
+}
---
+++ Assets/Scripts/itemLoad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class itemLoad : MonoBehaviour
+{
+    GameObject controller;
+    public string nameAsset, nameBundle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+        gameObject.GetComponent<Button>().onClick.AddListener(() => { controller.GetComponent<LoadAsset>().LoadLocal(nameAsset, nameBundle); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
